d410017b6efdc389f6e489d417025da934a3cd95
HyperBlitzer <josearreguin1676@gmail.com>
2022-02-04 14:22:27 -0800

    adding files + lib

--- randomStuff.java
+++ randomStuff.java
@@ -1,1 +1,6 @@
+public class randomStuff {
+	public static int multiplyNumbers(int x, int y){
+		return x * y;
+	}
+}
 
--- randomStuffTest.java
+++ randomStuffTest.java
@@ -1,1 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class randomStuffTest {
+	@Test 
+	public void testMultiplyNumbers(){
+		assertEquals(10, randomStuff.multiplyNumbers(3,3));
+	}
+}
